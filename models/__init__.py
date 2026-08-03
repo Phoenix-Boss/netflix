3ef0f69684bdab8017784f17bf2df63e1b4be16a
Zapier-codes <Zapier-codes@users.noreply.github.com>
2026-08-03 19:31:05 +0100

    auto versioning from git sha

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
-VERSION = "13.0.1"
+import os
+
+_base_version = "13.0.0"
+_commit = os.environ.get("RENDER_GIT_COMMIT_SHA", "")[:7]
+VERSION = f"{_base_version}+{_commit}" if _commit else _base_version
+
 from .vidapi import extract, format_sources, extract_quality
 from .subtitles import get_subtitles
 from .utils import fetch, error
